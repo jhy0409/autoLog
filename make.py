8797099d7810d1e2e40ff5bd525e930f96f40dba
jhy0409 <gdlk49@gmail.com>
2024-03-13 16:57:31 +0900

    2024-3-13

---
+++ make.py
@@ -0,0 +1,46 @@
+from subprocess import call 
+import datetime, os
+
+date = datetime.datetime.now()
+
+# Y=$(date +%Y)
+# M=$(date +%m)
+# D=$(date +%d)
+
+Y = date.year
+M = date.month
+D = date.day
+ 
+# Ym=$Y-$M
+Ymd=f'{Y}-{M}-{D}'
+GitRep="autoLog"
+ 
+HomeDir="/Users/jhy/Documents"
+GitDir="$HomeDir/$GitRep"
+FileNm="log.md"
+ 
+# echo f"Ymd" > $GitDir/$FileNm
+ 
+# cd $GitDir
+# git add .
+# git commit -m "$Ymd"
+
+# echo "" > $GitDir/$FileNm
+# git add .
+# git commit -m "$Ymd"
+
+# git push origin main
+directory = '/Users/jhy/Documents/autoLog'
+os.chdir(directory)
+
+file_path = os.path.join(directory,'log.md')
+
+content = f'{Ymd}'
+
+if os.path.exists(file_path):
+    with open(file_path, 'a') as file:
+        file.write(content)
+
+call('git add .', shell=True)
+call(['git', 'commit', '-m', Ymd])
+call('git push origin main', shell=True)
